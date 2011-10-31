926ae0bf28a35087c907ecef68523eca7ba6c631
Aiham Hammami <aiham@aiham.net>
2011-10-31 17:02:29 +0900

    Add option for formatting Xml string with new lines and indents

--- XmlFormat.php
+++ XmlFormat.php
@@ -18,22 +18,35 @@ class XmlFormat extends Format {
     return new XmlFormat($data);
   }
 
-  public function toString () {
-    $xml = new SimpleXmlElement('<xml/>');
+  public function toString ($format = false) {
+    $xml = new SimpleXmlElement('<root/>');
     if (is_array($this->data)) {
       $this->traverseElement($xml, $this->data);
     }
-    return $xml->asXML();
+    $string = $xml->asXML();
+    return $format ? static::format($string) : $string;
   }
 
   protected function traverseElement ($xml, $data) {
     foreach ($data as $key => $element) {
+      if (is_int($key)) {
+        $key = 'value' . $key;
+      }
       if (is_array($element)) {
         $this->traverseElement($xml->addChild($key), $element);
       } else {
         $xml->addChild($key, $element);
       }
     }
+  }
+
+  protected static function format ($string) {
+    $dom = new DOMDocument('1.0');
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    $dom->loadXML($string);
+    
+    return $dom->saveXML();
   }
 
 }
