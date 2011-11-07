f9c1adb8ebd4618cd21d7e1e32ebb01f89239cac
Aiham Hammami <aiham@aiham.net>
2011-11-07 08:23:17 +0000

    Fix CharactersetFormat to convert between character sets while storing data in UTF-8

--- lib/CharactersetFormat.php
+++ lib/CharactersetFormat.php
@@ -6,13 +6,15 @@ class CharactersetFormat extends Format {
 
   protected static $from = array('characterset'), $label = 'characterset';
 
-  public static function fromString ($string, $set) {
-    $data = icovnv($set, 'UTF-8', $string);
-    return $data === false ? false : new CharactersetFormat($data);
+  public static function fromString ($string, $set = 'UTF-8') {
+    if ($set !== 'UTF-8') {
+      $string = iconv($set, 'UTF-8', $string);
+    }
+    return $string === false ? false : new CharactersetFormat($string);
   }
 
   public function toString ($set = 'UTF-8') {
-    return serialize($this->data);
+    return $set !== 'UTF-8' ? iconv('UTF-8', $set, $this->data) : $this->data;
   }
 
 }
