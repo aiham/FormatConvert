6c7feee85b7bd87f2731cdfc98b5b6203c1125fb
Aiham Hammami <aiham@aiham.net>
2011-11-07 09:00:20 +0000

    Expand dispatcher to display different pages and errors

--- lib/Dispatcher.php
+++ lib/Dispatcher.php
@@ -1,18 +1,52 @@
 <?php
 
-require_once dirname(__FILE__) . '/Template.php';
+define('LIB_DIR', dirname(__FILE__) . '/');
+define('TEMPLATE_DIR', LIB_DIR . 'templates/');
+
+require_once LIB_DIR . 'Template.php';
 
 class Dispatcher {
 
   protected $template;
 
   public function __construct () {
-    $this->template = new Template();
   }
 
   public function dispatch () {
-    $this->setPath(dirname(__FILE__) . '/templates/index.php');
-    $this->display();
+    $this->template = new Template();
+
+    try {
+
+      if (strtolower($_POST['REQUEST_METHOD']) === 'post') {
+
+        $this->dispatch_convert();
+
+      } else {
+
+        $this->dispatch_index();
+
+      }
+
+    } catch (Exception $e) {
+
+      $this->dispatch_error($e->getMessage());
+
+    }
+
+    $this->template->display();
+  }
+
+  protected function dispatch_index () {
+    $this->template->setPath(TEMPLATE_DIR . 'index.php');
+  }
+
+  protected function dispatch_convert () {
+    $this->template->setPath(TEMPLATE_DIR . 'convert.php');
+  }
+
+  protected function dispatch_error ($message) {
+    $this->template->setPath(TEMPLATE_DIR . 'error.php');
+    $this->template->assign('message', $message);
   }
 
 }
